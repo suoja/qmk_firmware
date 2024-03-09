a843340f426275ec325cbe5d1209ea2042588f0a
Jarkko Suominen <jarkko@suominen.dev>
2024-03-09 18:49:20 +0200

    Disable RGB and OLEDs

--- keyboards/crkbd/keymaps/suoja/rules.mk
+++ keyboards/crkbd/keymaps/suoja/rules.mk
@@ -1,4 +1,4 @@
 RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
+RGB_MATRIX_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
 CONVERT_TO=liatris
