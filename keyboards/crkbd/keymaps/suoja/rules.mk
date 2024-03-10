07373e4c34fab15c1e15eb554a4f935e4c8ce1db
Jarkko Suominen <jarkko@suominen.dev>
2024-03-10 18:56:31 +0200

    Enable OLEDs

--- keyboards/crkbd/keymaps/suoja/rules.mk
+++ keyboards/crkbd/keymaps/suoja/rules.mk
@@ -1,6 +1,6 @@
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = no    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = no
+OLED_ENABLE     = yes
 CONVERT_TO=liatris
 UNICODE_COMMON = yes
 UNICODEMAP_ENABLE = yes
